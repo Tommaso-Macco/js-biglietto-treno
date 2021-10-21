5959da753d68291a9027414f328d45f2fea44e11
Tommaso <tommaso.macco@gmail.com>
2021-10-21 16:54:09 +0200

    macrostruttura js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+// Chiedere quanti km vuole percorrere
+
+
+// chiedere quanti anni ha
+
+// formula per i minorenno 20% di sconto
+
+// foruma per gli over 65 40% di sconto
+
+// calcolare se l'utente ha uno sconto o meno
+
+// mettere tutto a schermo
